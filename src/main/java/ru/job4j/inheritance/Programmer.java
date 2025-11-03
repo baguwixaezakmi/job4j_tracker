3d2f738bdeb12002e8b186deb683465573b6e90e
baguwixaezakmi <baguwixaezakmi@gmail.com>
2025-11-03 04:43:27 +0300

    3. Вызов конструктора родителя super(...)

--- src/main/java/ru/job4j/inheritance/Programmer.java
+++ src/main/java/ru/job4j/inheritance/Programmer.java
@@ -4,6 +4,7 @@ public class Programmer extends Engineer {
     private String programLanguage;
 
     public Programmer(String programLanguage, int experience, boolean degree) {
+        super(experience, degree);
         this.programLanguage = programLanguage;
     }
 }

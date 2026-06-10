157d6b75525a43a8052e1eb41804983a49382531
baguwixaezakmi <baguwixaezakmi@gmail.com>
2026-06-10 22:59:59 +0300

    0. Stream API улучшения

---
+++ src/main/java/ru/job4j/api/DropWhile.java
@@ -0,0 +1,12 @@
+package ru.job4j.api;
+
+import java.util.stream.Stream;
+
+public class DropWhile {
+    public static void main(String[] args) {
+        Stream.of(1, 2, 3, 4, 1, 2)
+            .dropWhile(value -> value < 3)
+            .map(value -> "Результат: " + value)
+            .forEach(System.out::println);
+    }
+}
---
+++ src/main/java/ru/job4j/api/TakeWhile.java
@@ -0,0 +1,12 @@
+package ru.job4j.api;
+
+import java.util.stream.Stream;
+
+public class TakeWhile {
+    public static void main(String[] args) {
+        Stream.of(1, 2, 3, 4)
+            .takeWhile(value -> value < 3)
+            .map(value -> "Результат: " + value)
+            .forEach(System.out::println);
+    }
+}
